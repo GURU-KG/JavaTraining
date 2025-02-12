244aa9db9eed044370b8e067f3c9ceb780899d78
Guru <127617090+GURU-KG@users.noreply.github.com>
2025-02-12 16:08:06 +0530

    ConditionCheck2.java

---
+++ ConditionCheck2.java
@@ -0,0 +1,14 @@
+class Main {
+    public static void main(String[] args) {
+        int a=10;
+        for( ; ; ){
+            if(a>3){
+            System.out.println(a);
+            }
+            else{
+                break;
+            }
+            a--;
+        }
+    }
+}
