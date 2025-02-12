7609bdac0d92591fb97a6bfcd323fd4c3f8bc080
Guru <127617090+GURU-KG@users.noreply.github.com>
2025-02-12 12:40:39 +0530

    BitwiseOp.java

---
+++ BitwiseOp.java
@@ -0,0 +1,13 @@
+class Bitwise{
+    public static void main(String []args)
+    {
+        int a = 392;
+        int b = 333;
+        int c = a&b;
+        int d = a|b;
+        int e = a^b;
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
