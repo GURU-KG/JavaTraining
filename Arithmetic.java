82d2a352a939d48017c2619f272b305bfc004e28
Guru <127617090+GURU-KG@users.noreply.github.com>
2025-02-12 11:05:59 +0530

    Arithmetic.java

---
+++ Arithmetic.java
@@ -0,0 +1,18 @@
+class Arithmetic{
+    public static void main(String []args)
+    {
+        byte a=10;
+        byte b=5;
+        byte sum= (byte)(a+b);
+        byte sub= (byte)(a-b);
+        byte mul= (byte)(a*b);
+        byte div= (byte)(a/b);
+        byte mod= (byte)(a%b);
+        
+        System.out.println("Addition:"+sum);
+         System.out.println("Subtraction:"+sub);
+          System.out.println("Multiplication:"+mul);
+           System.out.println("Division:"+div);
+            System.out.println("Modulo:"+mod);
+    }
+}
