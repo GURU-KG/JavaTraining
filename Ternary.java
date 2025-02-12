4a251bc5383f527aa34acbc0bd2ecd0c9750dfca
Guru <127617090+GURU-KG@users.noreply.github.com>
2025-02-12 12:04:11 +0530

    Ternary.java

---
+++ Ternary.java
@@ -0,0 +1,13 @@
+class Ternary{
+    public static void main(String []args)
+    {
+        int a=5;
+        int b=15;
+        int c=10;
+        int max=(a>b&&a>c)?a:(b>c)?b:c;
+        {
+            System.out.println(max+" is greater");
+        }
+        
+    }
+}
