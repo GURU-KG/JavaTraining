0118b503bfca6b464bddf6b251752197be91d532
Guru <127617090+GURU-KG@users.noreply.github.com>
2025-02-13 11:52:33 +0530

    For1.java

---
+++ For1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class For1{
+    public static void main(String[]args){
+        int i,j;
+        
+        int n =5;
+        for(i=1;i<=n;i++){
+            for(j=1;j<=i;j++){
+                System.out.print("1");
+            }System.out.println();
+        }
+    }
+}
