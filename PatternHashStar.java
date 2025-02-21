e876c6d91e13cd49e26df2410911cdcb7e7f3013
Guru <127617090+GURU-KG@users.noreply.github.com>
2025-02-21 15:13:14 +0530

    PatternHashStar.java

---
+++ PatternHashStar.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class PatternHashStar{
+static class Pattern{
+    static int n=5;
+    
+    static void display()
+    {
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                if(j%2==1)
+                {
+                System.out.print("*");
+                }
+                else{
+                    System.out.print("#");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+         
+
+
+	public static void main(String[] args) {
+	    Pattern.display();
+	}
+}                                       
